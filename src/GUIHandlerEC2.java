43fac2f8fdc07a695f315a1ad4a072a886dd4b92
Gregor Haas <gregorhaas1997@gmail.com>
2016-02-26 23:20:56 -0500

    Create GUIHandlerEC2.java

---
+++ src/GUIHandlerEC2.java
@@ -0,0 +1,56 @@
+import java.io.*;
+
+public class GUIHandler
+{
+    private static final int BOARDHEIGHT = 4;
+    private static final int BOARDWIDTH = 4;
+    private static final int SEARCHDEPTH = 4;
+
+    private static Board mainBoard;
+    private static PrintWriter fileWriter;
+    private static String name;
+
+    public static void main(String[] args)
+    {
+        name = BOARDHEIGHT + "x" + BOARDWIDTH + "-" + SEARCHDEPTH;
+
+        try
+        {
+            fileWriter = new PrintWriter(name + ".txt", "UTF-8");
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+
+        int trial = 0;
+
+        while(trial < 100)
+        {
+            trial++;
+            mainBoard = new Board(BOARDHEIGHT, BOARDWIDTH, 2);
+
+            while(!mainBoard.gameOver())
+            {
+                mainBoard.performOptimalMove(SEARCHDEPTH);
+                //update_display();
+            }
+
+            String dataString = trial + "\t" + SEARCHDEPTH + '\t' + mainBoard.getScore() + '\t'
+                    + mainBoard.getNumMoves() + '\t' + (int) Math.pow(2, mainBoard.getMaxNum()) + '\n';
+
+            //outputString += dataString;
+            //update_output();
+            
+            System.out.println(dataString);
+            fileWriter.println(dataString);
+            fileWriter.flush();
+        }
+
+        fileWriter.close();
+    }
+}
